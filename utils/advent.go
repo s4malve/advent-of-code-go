a22e9b3b1f0bc899b791daa5da5020151527ba4f
s4malve <s4malve@gmail.com>
2023-08-10 10:50:12 -0500

    refactor: extract advent utils in their file

--- utils/advent.go
+++ utils/advent.go
@@ -53,3 +53,48 @@ func Fatal(err error) {
 	}
 
 }
+
+func CreateFile(name, filePath, content string) {
+	mainFilePath := path.Join(filePath, name)
+	fmt.Printf("opening or creating %s file...\n", name)
+	f, err := os.OpenFile(mainFilePath, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("opening or creating %s file: %v\n", name, err)
+	}
+	fmt.Printf("file %s created\n", name)
+
+	defer f.Close()
+	fmt.Printf("writing %s file...\n", name)
+	_, err = f.Write([]byte(content))
+
+	if err != nil {
+		log.Fatalf("writing in %s file: %v\n", name, err)
+	}
+	fmt.Printf("%s file created\n", name)
+}
+
+func CreateFolder(name, path string) {
+	fmt.Printf("Creating %s folder...\n", name)
+	err := os.Mkdir(path, os.ModePerm)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s folder created\n", name)
+}
+
+func PathExists(path string) bool {
+	if _, err := os.Stat(path); os.IsExist(err) {
+		return true
+	}
+
+	return false
+}
+
+func RequireFlag(name string, value uint) {
+	if value == 0 {
+		log.Fatalf("flag -%s or -%c is required\n", name, name[0])
+	}
+}
